8babffe91ed57ba7294ba938c6dd12f0d97f6449
navdeep <navdeepst.1987@gmail.com>
2023-12-05 18:24:32 +1100

    Display articles based on search text

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Search from '../Search';
+import Article from '../Article';
+import './Home.css';
+
+const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
 
   const handleSearch = (searchTerm) => {
-    // Perform the search logic here
+    fetch(`https://newsapi.org/v2/everything?q=${searchTerm}`, {
+      headers: {
+        "X-Api-Key": NEWS_API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => setArticles(response.articles))
+      .catch((error) => console.log(error));
   };
 
-  return <Search onSearch={handleSearch} />
+  return (
+    <>
+      <Search onSearch={handleSearch} />
+      <div className="articles">
+      {articles.length > 0 &&
+        articles.map((article) => (
+          <Article
+            article={article}
+            key={article.title + article.source.name + article.publishedAt}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
