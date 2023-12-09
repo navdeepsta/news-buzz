e388f71de1544ebd1b40a8ce27a28b4c724e7a0d
navdeep <navdeepst.1987@gmail.com>
2023-12-09 11:58:53 +1100

    Add ReadingList state in Home and pass it to Articles for updation

--- src/Articles/index.js
+++ src/Articles/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import Article from "../Article";
 import "./Articles.css";
 
-function Articles({ articles }) {
+function Articles({ articles, onSetReadingList }) {
   return (
     <div className="articles">
       {articles.length > 0 &&
         articles.map(
           (article) =>
-            article.author && (
+            article.urlToImage  && (
               <Article
                 article={article}
                 key={article.title + article.source.name + article.publishedAt}
+                onSetReadingList={onSetReadingList}
               />
             )
         )}
--- src/Home/index.js
+++ src/Home/index.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Search from "../Search";
 import Articles from "../Articles";
 import Categories from "../Categories";
+import { readingListDB } from "../ReadingListDB/ReadingListDB";
 
 const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 export default function Home() {
   const [articles, setArticles] = useState([]);
+  const [ readingList, setReadingList ] = useState(readingListDB.readFromReadingList());
 
   useEffect(() => {
     fetch(`https://newsapi.org/v2/top-headlines?language=en&category=general`, {
@@ -19,7 +21,7 @@ export default function Home() {
       })
       .catch((error) => console.log(error));
 
-  }, [])
+  }, []);
 
   const handleSearch = (searchTerm) => {
     fetch(`https://newsapi.org/v2/everything?q=${searchTerm}`, {
@@ -36,10 +38,11 @@ export default function Home() {
 
   return (
     <div className="App">
+      <p>ReadingList: {readingList.length} Articles</p>
       <h1>News Buzz</h1>
       <Search onSearch={handleSearch} />
       <Categories />
-      <Articles articles={articles} />
+      <Articles articles={articles} onSetReadingList={(readingList)=>setReadingList(readingList)} />
     </div>
   );
 }
