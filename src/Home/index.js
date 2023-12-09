6107a391998e1f3eb9a00603c5ff6af79b54eb20
navdeep <navdeepst.1987@gmail.com>
2023-12-09 13:57:10 +1100

    Modify Home to include loading and add link to ReadingList

--- src/Home/index.js
+++ src/Home/index.js
@@ -3,11 +3,14 @@ import Search from "../Search";
 import Articles from "../Articles";
 import Categories from "../Categories";
 import { readingListDB } from "../ReadingListDB/ReadingListDB";
+import { Link } from "react-router-dom";
 
 const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 export default function Home() {
   const [articles, setArticles] = useState([]);
-  const [ readingList, setReadingList ] = useState(readingListDB.readFromReadingList());
+  const [readingList, setReadingList] = useState(
+    readingListDB.readFromReadingList()
+  );
 
   useEffect(() => {
     fetch(`https://newsapi.org/v2/top-headlines?language=en&category=general`, {
@@ -20,7 +23,6 @@ export default function Home() {
         setArticles(response.articles);
       })
       .catch((error) => console.log(error));
-
   }, []);
 
   const handleSearch = (searchTerm) => {
@@ -38,11 +40,20 @@ export default function Home() {
 
   return (
     <div className="App">
-      <p>ReadingList: {readingList.length} Articles</p>
+      <Link to={`/readingList`}>
+        ReadingList: {readingList.length} Articles
+      </Link>
       <h1>News Buzz</h1>
       <Search onSearch={handleSearch} />
       <Categories />
-      <Articles articles={articles} onSetReadingList={(readingList)=>setReadingList(readingList)} />
+      {articles.length ? (
+        <Articles
+          articles={articles}
+          onSetReadingList={(readingList) => setReadingList(readingList)}
+        />
+      ) : (
+        <span className="loading">Loading...</span>
+      )}
     </div>
   );
 }
