f568c24f6fb7b2be73e7cfa5fc07858521c2dc80
navdeep <navdeepst.1987@gmail.com>
2023-12-09 13:51:11 +1100

    Modify App.js to add route to ReadingList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Home';
 import Category from './Category';
 import Source from './Source';
+import ReadingList from './ReadingList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/category/:category" element={<Category />}>
           <Route path="/category/:category/source/:source" element={<Source />} />
         </Route>
+        <Route path="/readingList" element={<ReadingList />}/>
       </Routes>
     </BrowserRouter>
   );
